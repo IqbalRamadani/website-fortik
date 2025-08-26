f4d57b60a619fb16cc12611c51854573e0ad1f00
IqbalRamadani <hakimramadani24@gmail.com>
2025-08-26 16:59:53 +0700

    misahin navlink

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -31,10 +31,10 @@
         <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                 <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Beranda</a>
+                    <x-nav-link href="/" :active="request()->is('/')">Beranda</x-nav-link>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Tentang</a>
+                    <x-nav-link href="/about" :active="request()->is('about')">Tentang</x-nav-link>
                 </li>
                 {{-- deactivate dropdown menu --}}
                 {{-- <li>
@@ -64,13 +64,13 @@
                     </div>
                 </li> --}}
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Berita</a>
+                    <x-nav-link href="/news" :active="request()->is('news')">Berita</x-nav-link>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">FAQ</a>
+                    <x-nav-link href="/gallery" :active="request()->is('gallery')">Galeri</x-nav-link>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Kontak</a>
+                    <x-nav-link href="/contact" :active="request()->is('contact')">Kontak</x-nav-link>
                 </li>
             </ul>
         </div>
