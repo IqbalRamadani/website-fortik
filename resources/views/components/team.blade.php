24ab2cc41ede040350f20cf581d84ec79283566c
IqbalRamadani <hakimramadani24@gmail.com>
2025-08-26 13:56:27 +0700

    mengubah team card pake border

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -8,76 +8,76 @@
         </div>
     
         <div class="grid md:grid-cols-2 lg:grid-cols-4 place-items-center gap-8">
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Ketua FORTIK</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Muhammad Latief</p>
-                <p class="text-sm text-gray-100">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Latief</p>
+                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
             </div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Wakil Ketua</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Iqbal Ramadani</p>
-                <p class="text-sm text-gray-100">Mendampingi ketua dalam menjalankan organisasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Iqbal Ramadani</p>
+                <p class="text-sm text-gray-800">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Sekretaris</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Muhammad Hassan</p>
-                <p class="text-sm text-gray-100">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Hassan</p>
+                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
             </div>
 
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Bendahara</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Reza</p>
-                <p class="text-sm text-gray-100">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Reza</p>
+                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
             </div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Desain Grafis</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Faruq</p>
-                <p class="text-sm text-gray-100">Mendampingi ketua dalam menjalankan organisasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Faruq</p>
+                <p class="text-sm text-gray-700">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Videografi</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Sinar</p>
-                <p class="text-sm text-gray-100">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Sinar</p>
+                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
             </div>
 
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Konten Kreator</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Sadio</p>
-                <p class="text-sm text-gray-100">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Sadio</p>
+                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
             </div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PSDM</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Aiman</p>
-                <p class="text-sm text-gray-100">Mendampingi ketua dalam menjalankan organisasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Aiman</p>
+                <p class="text-sm text-gray-700">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
 
             <div class="hidden lg:block"></div>
     
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PR</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Rafif</p>
-                <p class="text-sm text-gray-100">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Rafif</p>
+                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
             </div>
 
-            <div class="size-full bg-endeavour/95 rounded-lg shadow-lg p-6 text-center">
+            <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv SIIP</h3>
-                <p class="text-gray-200 text-lg font-semibold mb-2">Ardan Omar</p>
-                <p class="text-sm text-gray-100">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Ardan Omar</p>
+                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
             </div>
         </div>
     </div>
