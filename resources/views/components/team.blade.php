8a3ccdc42d6d900d888d449f03e6ce41a1ccb2b1
IqbalRamadani <hakimramadani24@gmail.com>
2025-08-27 09:45:45 +0700

    nambahin gambar per orang

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-white">
-    <div class="max-w-7xl mx-auto px-12 py-6 lg:px-8 lg:py-16">
+    <div class="max-w-7xl mx-auto mb-4 px-12 py-6 lg:px-8 lg:py-16">
         <div class="text-center mb-16">
             <h2 class="text-3xl md:text-4xl font-bold text-gray-900 mb-4">Pengurus FORTIK</h2>
             <p class="text-gray-900 text-lg max-w-2xl mx-auto">
@@ -9,75 +9,85 @@
     
         <div class="grid md:grid-cols-2 lg:grid-cols-4 place-items-center gap-8">
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 mx-auto mb-4 rounded-full">
+                    <img src="{{ asset('images/img-0.png') }}" alt="latif" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Ketua FORTIK</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">M. Latif Hidayat</p>
-                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Latif</p>
             </div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-2.png') }}" alt="iqbal" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Wakil Ketua</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Iqbal Ramadani</p>
-                <p class="text-sm text-gray-800">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-3.png') }}" alt="hasan" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Sekretaris</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">M. Hasan Al Hakim</p>
-                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Hasan</p>
             </div>
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-4.png') }}" alt="reza" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Bendahara</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Reza Ahmad</p>
-                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
             </div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-5.png') }}" alt="faruq" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Desain Grafis</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Faruq Taqiyuddin</p>
-                <p class="text-sm text-gray-700">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-6.png') }}" alt="sinar" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Videografi</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">Sinar Surya Sakti</p>
-                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Sinar Surya</p>
             </div>
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-7.png') }}" alt="syadio" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Konten Kreator</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">M. Syadio Candra</p>
-                <p class="text-sm text-gray-700">Memimpin dan mengkoordinasi semua kegiatan FORTIK</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Syadio</p>
             </div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-8.png') }}" alt="aiman" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PSDM</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Aiman Abdurrahman</p>
-                <p class="text-sm text-gray-700">Mendampingi ketua dalam menjalankan organisasi</p>
             </div>
 
             <div class="hidden lg:block"></div>
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-9.png') }}" alt="rafif" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PR</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">M. Rafif Abdul Aziz</p>
-                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Rafif</p>
             </div>
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
-                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"></div>
+                <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
+                    <img src="{{ asset('images/img-10.png') }}" alt="ardan" class="rounded-full">
+                </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv SIIP</h3>
-                <p class="text-gray-800 text-lg font-semibold mb-2">Ardan Omar Wildan</p>
-                <p class="text-sm text-gray-700">Mengelola administrasi dan dokumentasi</p>
+                <p class="text-gray-800 text-lg font-semibold mb-2">Ardan Omar</p>
             </div>
         </div>
     </div>
