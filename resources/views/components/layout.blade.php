73c079dcb7dcc97b3e73c69f28beb05aa6a7cb9a
IqbalRamadani <hakimramadani24@gmail.com>
2025-08-27 09:32:58 +0700

    mengubah title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @vite('resources/css/app.css')
-        <title>Document</title>
+        <title>FORTIK STDIIS</title>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </head>
