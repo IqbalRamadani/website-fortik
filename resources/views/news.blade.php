287a8cbc3c5aa9d0f4d9d7df8b5ce552961a795b
IqbalRamadani <hakimramadani24@gmail.com>
2025-08-27 09:35:17 +0700

    nambahin pesan proses pengembangan

---
+++ resources/views/news.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+  <h1 class="w-full h-screen flex justify-center items-center font-bold text-3xl mx-auto p-20 text-center bg-gradient-to-r from-koamaru to-matcha text-gray-200">Halaman sedang dalam proses pengembangan</h1>
+</x-layout>
