0e2a24bf6dc8f2258ebdaae51c87cae5b731830e
IqbalRamadani <hakimramadani24@gmail.com>
2025-10-26 08:22:59 +0700

    menambah singkatan STDI

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -6,7 +6,7 @@ style="background-image: url({{ asset('images/bg-hero.png') }})"
     </div>
     <div class="mx-auto mb-12 md:mb-4 text-center lg:col-span-8">
         <h1 class="max-w-3xl mb-4 text-2xl font-extrabold tracking-tight leading-none text-white md:text-5xl xl:text-6xl">Forum Teknologi Informasi dan Komunikasi</h1>
-        <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i Jember - Jawa Timur.</p>
+        <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDI&rpar; Jember - Jawa Timur.</p>
         <a href="https://t.me/kanalfortikstdiis" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-primary-800 hover:bg-primary-900 focus:ring-4 focus:ring-primary-300">
             Gabung
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram ml-2" viewBox="0 0 16 16">
