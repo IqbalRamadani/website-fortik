1cc1bfdceb7d9f154719546eaedaf653006665f4
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-14 11:58:43 +0700

    inter as a main font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
         <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </head>
-    <body class="font-display">
+    <body>
         <div class="max-w-screen min-h-full">
             <x-navbar></x-navbar>
             <main>
