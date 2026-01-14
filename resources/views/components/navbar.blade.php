32b1fcb9782ce580351ac5b2734a99bf95656650
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-14 13:36:02 +0700

    modifying navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
-<nav class="bg-white border-gray-200 fixed w-full z-20 top-0 start-0 border-b">
-    <div class="flex max-w-2xl md:max-w-3xl lg:max-w-7xl flex-wrap items-center justify-between mx-auto px-4 py-6 lg:px-12 lg:py-4">
+<nav class="bg-linear-to-br from-lkoamaru to-koamaru fixed w-full z-100 top-0 start-0 shadow-lg overflow-hidden">
+    <div class="flex max-w-2xl md:max-w-3xl lg:max-w-7xl flex-wrap items-center justify-between mx-auto px-6 py-4 md:px-16 lg:px-24">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="{{ asset('images/logo-fortik-3b.png') }}" class="w-[124px] h-[40px]" alt="Logo Fortik" />
+            <img src="{{ asset('images/logo-navbar.png') }}" class="w-[93px] h-[30px] lg:w-[124px] lg:h-[40px]" alt="Logo Fortik" />
         </a>
-        <div class="flex md:order-2">
+        <div>
             {{-- Search deactivate --}}
             {{-- <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="lg:hidden text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm p-2.5 me-1">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -20,7 +20,7 @@
                 </div>
                 <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
             </div> --}}
-            <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+            <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -29,12 +29,12 @@
         </div>
 
         <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+            <ul class="flex flex-col font-semibold text-sm lg:text-base p-0 mt-4 bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                 <li>
-                    <x-nav-link href="/" :active="request()->is('/')">Beranda</x-nav-link>
+                    <x-nav-link href="/" :active="request()->is('/')">BERANDA</x-nav-link>
                 </li>
                 <li>
-                    <x-nav-link href="/about" :active="request()->is('about')">Tentang</x-nav-link>
+                    <x-nav-link href="/about" :active="request()->is('about')">TENTANG</x-nav-link>
                 </li>
                 {{-- deactivate dropdown menu --}}
                 {{-- <li>
@@ -64,13 +64,13 @@
                     </div>
                 </li> --}}
                 <li>
-                    <x-nav-link href="/post" :active="request()->is('post')">ForNews</x-nav-link>
+                    <x-nav-link href="/post" :active="request()->is('post')">FORNEWS</x-nav-link>
                 </li>
                 <li>
-                    <x-nav-link href="/gallery" :active="request()->is('gallery')">Galeri</x-nav-link>
+                    <x-nav-link href="/gallery" :active="request()->is('gallery')">GALERI</x-nav-link>
                 </li>
                 <li>
-                    <x-nav-link href="/bulletin" :active="request()->is('bulletin')">ForSight</x-nav-link>
+                    <x-nav-link href="/bulletin" :active="request()->is('bulletin')">FORSIGHT</x-nav-link>
                 </li>
                 {{-- <li> 
                     <x-nav-link href="/announcement" :active="request()->is('announcement')">Pengumuman</x-nav-link>
