7275e3d962a5fd1388a8f96d2ed4356dc345f1b2
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-14 09:22:21 +0700

    changing color admin n adding collapse sidebar

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,12 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->sidebarCollapsibleOnDesktop()
+                ->brandLogo(asset('images/logo-panel.png'))
+                ->brandLogoHeight('2rem')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
