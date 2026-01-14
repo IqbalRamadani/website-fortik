6b37425174df6ef2944f9edd004e26c5a4106921
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-14 08:44:49 +0700

    adding post model and migration

---
+++ app/Models/post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class post extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 
+        'slug', 
+        'user_id', 
+        'published_at', 
+        'content', 
+        'image',
+        'status',
+    ];
+
+    protected $casts = [
+        'published_at' => 'date',
+    ];
+
+    protected $with = ['author'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
