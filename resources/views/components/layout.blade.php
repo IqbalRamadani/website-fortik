383db29ae3cde016ae4849aaae68df4ce928cd52
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-14 11:07:12 +0700

    adding favicon logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @vite('resources/css/app.css')
         <title>FORTIK STDIIS</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo-favicon.png') }}">
         <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </head>
