b518cb4c780eb8f5ffc0e91d05ca7f4da904edfd
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-17 05:54:50 +0700

    sizing faq accordion

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -1,46 +1,32 @@
 <div class="bg-white px-6 py-8 md:py-12 lg:py-16">
-    <div class="w-full max-w-7xl mx-auto text-center">
-        <h2 class="text-3xl md:text-4xl font-bold text-koamaru mb-8">FAQ</h2>
-        <div class="collapse collapse-arrow bg-base-100 border border-base-300 rounded-none mb-2 shadow-lg">
+    <div class="w-full max-w-7xl mx-auto">
+        <h2 class="text-3xl md:text-4xl font-bold text-center text-koamaru mb-8">FAQ</h2>
+        <div class="collapse collapse-arrow bg-base-100 border border-koamaru rounded-none mb-2 shadow-lg">
             <input type="radio" name="my-accordion-2" checked="checked" />
-            <div class="collapse-title font-semibold bg-koamaru text-white">How do I create an account?</div>
+            <div class="collapse-title font-semibold text-sm bg-koamaru text-white">Kapan FORTIK membuka pendaftaran anggota baru?</div>
             <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="py-4">
-                Click the "Sign Up" button in the top right corner and follow the registration process.
+                <div class="mt-2 py-2">
+                Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
                 </div>
             </div>
         </div>
-        <div class="collapse collapse-arrow bg-base-100 border border-base-300 rounded-none mb-2 shadow-lg">
+        <div class="collapse collapse-arrow bg-base-100 border border-koamaru rounded-none mb-2 shadow-lg">
             <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold bg-koamaru text-white">I forgot my password. What should I do?</div>
+            <div class="collapse-title font-semibold text-sm bg-koamaru text-white">FORTIK itu ngapain sih?</div>
             <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="py-4">
-                Click on "Forgot Password" on the login page and follow the instructions sent to your email.
+                <div class="mt-2 py-2">
+                FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
                 </div>
             </div>
         </div>
-        <div class="collapse collapse-arrow bg-base-100 border border-base-300 rounded-none mb-2 shadow-lg">
+        <div class="collapse collapse-arrow bg-base-100 border border-koamaru rounded-none mb-2 shadow-lg">
             <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold bg-koamaru text-white">How do I update my profile information?</div>
+            <div class="collapse-title font-semibold text-sm bg-koamaru text-white">Apa keuntungan masuk FORTIK?</div>
             <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="py-4">
-                Go to "My Account" settings and select "Edit Profile" to make changes.
+                <div class="mt-2 py-2">
+                Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
                 </div>
             </div>
         </div>
-        {{-- <div class="text-left space-y-6">
-            <div class="bg-lkoamaru/10 backdrop-blur-sm rounded-lg p-6">
-                <h3 class="text-xl font-semibold text-koamaru mb-2">Kapan FORTIK membuka pendaftaran anggota baru?</h3>
-                <p class="text-koamaru/90">Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!</p>
-            </div>
-            <div class="bg-lkoamaru/10 backdrop-blur-sm rounded-lg p-6">
-                <h3 class="text-xl font-semibold text-koamaru mb-2">FORTIK itu ngapain sih?</h3>
-                <p class="text-koamaru/90">FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.</p>
-            </div>
-            <div class="bg-lkoamaru/10 backdrop-blur-sm rounded-lg p-6">
-                <h3 class="text-xl font-semibold text-koamaru mb-2">Apa keuntungan masuk FORTIK?</h3>
-                <p class="text-koamaru/90">Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.</p>
-            </div>
-        </div> --}}
     </div>
 </div>
