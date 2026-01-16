039103ac0d293be4627eb1b537adacf78f44f93f
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-17 05:13:04 +0700

    sizing padding for mobile

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,13 +1,13 @@
 <div 
-    class="flex mx-auto min-h-screen bg-cover bg-center justify-items-center items-center px-6 py-6 md:px-16 lg:py-8 hidden-overflow" 
+    class="flex mx-auto min-h-screen bg-cover bg-center justify-items-center items-center px-6 py-4 hidden-overflow" 
     style="background-image: url('{{ asset('images/bg-tentang.png') }}');"
 >
-    <div class="max-w-7xl mx-auto px-12 py-6 lg:px-8 lg:py-16">
-        <div class="text-center mb-16">
-            <img src="{{ asset('images/logo-fortik-3b.png') }}" alt="Logo Fortik" loading="lazy" class="w-[250px] h-[78px] md:w-[375px] md:h-[117px] lg:w-[500px] lg:h-[156px] mx-auto mb-6">
-            <h2 class="text-xl font-semibold italic underline text-koamaru mb-6 md:mb-8 lg:mb-12 md:text-2xl lg:text-4xl">#Develop Your Creativity</h2>
-            <p class="text-koamaru text-base md:text-lg max-w-3xl mx-auto">
-                FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur. Sebagai Unit Kegiatan Mahasiswa &lpar;UKM&rpar;, FORTIK bertujuan untuk mengembangkan pengetahuan dan keterampilan anggotanya dalam bidang teknologi informasi dan komunikasi melalui berbagai kegiatan seperti workshop, seminar, dan pelatihan.
+    <div class="mx-auto">
+        <div class="text-center mb-4">
+            <img src="{{ asset('images/logo-fortik-3b.png') }}" alt="Logo Fortik" loading="lazy" class="w-[250px] h-[78px] md:w-[375px] md:h-[117px] lg:w-[500px] lg:h-[156px] mx-auto mb-4">
+            <h2 class="text-xl font-semibold italic underline text-koamaru mb-6 md:text-2xl lg:text-4xl">#Develop Your Creativity</h2>
+            <p class="text-koamaru text-base text-justify md:text-lg mx-auto">
+                FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur.
             </p>
         </div>
     </div>
