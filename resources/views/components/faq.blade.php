995028b54b383cbe6d88364cbce4eb7ecd8bd3e8
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 09:03:47 +0700

    adding padding for collapse title n content

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -1,31 +1,49 @@
-<div class="px-6 py-8 md:py-12 lg:py-16 bg-white bg-faq-mobile md:bg-faq bg-cover bg-top border-none">
+<div class="px-6 py-12 md:py-16 lg:py-36 bg-white bg-faq-mobile md:bg-faq bg-cover bg-top border-none">
     <div class="w-full max-w-7xl mx-auto">
-        <h2 class="text-3xl md:text-4xl font-bold text-center text-koamaru -mt-8 md:-mt-12 mb-8">FAQ</h2>
-        <div class="w-full md:max-w-xl lg:max-w-3xl">
-            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+        <h2 class="text-3xl md:text-4xl lg:text-6xl font-bold text-center text-koamaru -mt-8 md:-mt-12 mb-8">FAQ</h2>
+        <div class="w-full md:max-w-xl lg:max-w-4xl">
+            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
                 <input type="radio" name="my-accordion-2" checked="checked" />
-                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Kapan FORTIK membuka pendaftaran anggota baru?</div>
-                <div class="collapse-content text-sm bg-white text-koamaru">
-                    <div class="mt-2 py-2">
-                    Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
+                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
+                    <div class="px-6 lg:px-12">
+                        <span class="font-semibold text-sm lg:text-lg text-white">Kapan FORTIK membuka pendaftaran anggota baru?</span>
+                    </div>
+                </div>
+                <div class="collapse-content bg-white">
+                    <div class="mt-2 px-6 lg:px-12 py-2">
+                        <span class="text-sm lg:text-base text-koamaru text-wrap">
+                            Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
+                        </span>
                     </div>
                 </div>
             </div>
-            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
                 <input type="radio" name="my-accordion-2" />
-                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">FORTIK itu ngapain sih?</div>
-                <div class="collapse-content text-sm bg-white text-koamaru">
-                    <div class="mt-2 py-2">
-                    FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
+                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
+                    <div class="px-6 lg:px-12">
+                        <span class="font-semibold text-sm lg:text-lg text-white">FORTIK itu ngapain sih?</span>
+                    </div>
+                </div>
+                <div class="collapse-content bg-white">
+                    <div class="mt-2 px-6 lg:px-12 py-2">
+                        <span class="text-sm lg:text-base text-koamaru">
+                            FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
+                        </span>
                     </div>
                 </div>
             </div>
-            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
                 <input type="radio" name="my-accordion-2" />
-                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Apa keuntungan masuk FORTIK?</div>
-                <div class="collapse-content text-sm bg-white text-koamaru">
-                    <div class="mt-2 py-2">
-                    Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
+                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
+                    <div class="px-6 lg:px-12">
+                        <span class="font-semibold text-sm lg:text-lg text-white">Apa keuntungan masuk FORTIK?</span>
+                    </div>
+                </div>
+                <div class="collapse-content bg-white">
+                    <div class="mt-2 px-6 lg:px-12 py-2">
+                        <span class="text-sm lg:text-base text-koamaru">
+                            Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
+                        </span>
                     </div>
                 </div>
             </div>
