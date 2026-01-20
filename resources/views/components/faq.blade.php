45f5bc28c5e72dcc5b2d78715c4e3d41bac1dcc3
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 08:02:13 +0700

    adding max w for faq

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -1,30 +1,32 @@
 <div class="px-6 py-8 md:py-12 lg:py-16 bg-white bg-faq-mobile md:bg-faq bg-cover bg-top border-none">
-    <h2 class="text-3xl md:text-4xl font-bold text-center text-koamaru -mt-8 md:-mt-12 mb-8">FAQ</h2>
-    <div class="w-full md:max-w-xl lg:max-w-3xl">
-        <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
-            <input type="radio" name="my-accordion-2" checked="checked" />
-            <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Kapan FORTIK membuka pendaftaran anggota baru?</div>
-            <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="mt-2 py-2">
-                Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
+    <div class="w-full max-w-7xl mx-auto">
+        <h2 class="text-3xl md:text-4xl font-bold text-center text-koamaru -mt-8 md:-mt-12 mb-8">FAQ</h2>
+        <div class="w-full md:max-w-xl lg:max-w-3xl">
+            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+                <input type="radio" name="my-accordion-2" checked="checked" />
+                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Kapan FORTIK membuka pendaftaran anggota baru?</div>
+                <div class="collapse-content text-sm bg-white text-koamaru">
+                    <div class="mt-2 py-2">
+                    Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
-            <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">FORTIK itu ngapain sih?</div>
-            <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="mt-2 py-2">
-                FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
+            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+                <input type="radio" name="my-accordion-2" />
+                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">FORTIK itu ngapain sih?</div>
+                <div class="collapse-content text-sm bg-white text-koamaru">
+                    <div class="mt-2 py-2">
+                    FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
-            <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Apa keuntungan masuk FORTIK?</div>
-            <div class="collapse-content text-sm bg-white text-koamaru">
-                <div class="mt-2 py-2">
-                Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
+            <div class="collapse collapse-arrow bg-base-100 rounded-none mb-2 shadow-xl">
+                <input type="radio" name="my-accordion-2" />
+                <div class="collapse-title font-semibold text-sm bg-linear-to-br from-lkoamaru to-koamaru text-white">Apa keuntungan masuk FORTIK?</div>
+                <div class="collapse-content text-sm bg-white text-koamaru">
+                    <div class="mt-2 py-2">
+                    Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
+                    </div>
                 </div>
             </div>
         </div>
