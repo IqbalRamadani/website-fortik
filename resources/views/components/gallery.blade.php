36da2f4bc1b626cee025ad3b44760ef6a606dd69
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 15:51:20 +0700

    Changing accordion daisy to sweeper js

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -1,96 +1,71 @@
 <div class="bg-white px-6 py-8 md:py-12 lg:py-16">
     <div class="w-full max-w-6xl mx-auto">
         <h2 class="mx-auto -mt-4 p-2 text-center text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-4 md:mb-6 lg:mb-8">Galeri</h2>
-        <div class="flex justify-center w-full max-w-sm md:max-w-4xl lg:max-w-7xl overflow-x-hidden">
-            <div class="carousel">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-volunteer-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide7" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide2" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
+        <div class="flex justify-center w-full max-w-sm md:max-w-4xl lg:max-w-7xl">
+            {{-- Swiper --}}
+            <div class="swiper mySwiper w-full max-w-sm md:max-w-4xl lg:max-w-7xl mx-auto">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-volunteer-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-pemilu-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-opening-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-mubes-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-makrab-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-libre-1.jpg') }}" class="w-full object-cover" />
+                    </div>
+                    <div class="swiper-slide relative">
+                        <img src="{{ asset('images/g-mubes-2.jpg') }}" class="w-full object-cover" />
                     </div>
                 </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-pemilu-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide1" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide3" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
+
+                {{-- Swipper Arrow Button --}}
+                <div class="swiper-button-prev !w-8 !h-8 md:!w-12 md:!h-12 lg:!w-14 lg:!h-14 bg-white/45 hover:bg-gray-900 !text-black hover:!text-white flex items-center justify-center transition-all duration-300 rounded-none !left-2 md:!left-4">
+                    <span class="text-sm md:text-lg lg:text-xl font-bold">&lang;</span>
                 </div>
-                <div id="slide3" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-opening-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide2" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide4" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
+
+                <div class="swiper-button-next !w-8 !h-8 md:!w-12 md:!h-12 lg:!w-14 lg:!h-14 bg-white/45 hover:bg-gray-900 !text-black hover:!text-white flex items-center justify-center transition-all duration-300 rounded-none !right-2 md:!right-4">
+                    <span class="text-sm md:text-lg lg:text-xl font-bold">&rang;</span>
                 </div>
-                <div id="slide4" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-mubes-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide3" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide5" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
-                </div>
-                <div id="slide5" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-makrab-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide4" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide6" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
-                </div>
-                <div id="slide6" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-libre-1.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide5" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide7" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
-                </div>
-                <div id="slide7" class="carousel-item relative w-full">
-                    <img
-                    src="{{ asset('images/g-mubes-2.jpg') }}"
-                    class="w-full" />
-                    <div class="absolute left-2 right-2 md:left-4 md:right-4 lg:left-8 lg:right-8 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide6" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&lang;</a>
-                        <a href="#slide1" class="btn btn-rectangle w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 bg-white/45 text-black text-sm md:text-lg lg:text-xl hover:text-white hover:bg-gray-900">&rang;</a>
-                    </div>
-                </div>
+
+                {{-- Swiper Pagination --}}
+                <div class="swiper-pagination"></div>
             </div>
+            {{-- End Swiper --}}
         </div>
     </div>
 
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const carouselButtons = document.querySelectorAll('.carousel .btn');
+        var swiper = new Swiper(".mySwiper", {
+            loop: true, 
+            
+            autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            },
 
-            carouselButtons.forEach(btn => {
-                btn.addEventListener('click', function(e) {
-                    e.preventDefault();
+            speed: 800,
+            slidesPerView: 1,
+            spaceBetween: 0,
 
-                    const targetId = this.getAttribute('href').substring(1);
-                    const targetSlide = document.getElementById(targetId);
-                    
-                    if (targetSlide) {
-                        const container = targetSlide.parentElement;
-                        
-                        container.scrollTo({
-                            left: targetSlide.offsetLeft,
-                            behavior: 'smooth'
-                        });
-                    }
-                });
-            });
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            },
         });
     </script>
 </div>
