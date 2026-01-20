ccaa38c4e9108051f897e284e8d2a79cc8cf90ef
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 08:05:32 +0700

    adding max w for gallery

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -1,70 +1,70 @@
 <div class="bg-white px-6 py-8 md:py-12 lg:py-16">
-    <div class="mx-auto -mt-4 p-2 text-center">
-        <h2 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-4 md:mb-6 lg:mb-8">Galeri</h2>
-    </div>
-    <div class="flex justify-center w-full max-w-sm md:max-w-4xl lg:max-w-7xl overflow-x-hidden">
-        <div class="carousel">
-            <div id="slide1" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-volunteer-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide7" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 bg-transparent text-black hover:text-white hover:bg-gray-900">&lang;</a>
-                    <a href="#slide2" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+    <div class="w-full max-w-7xl mx-auto">
+        <h2 class="mx-auto -mt-4 p-2 text-center text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-4 md:mb-6 lg:mb-8">Galeri</h2>
+        <div class="flex justify-center w-full max-w-sm md:max-w-4xl lg:max-w-7xl overflow-x-hidden">
+            <div class="carousel">
+                <div id="slide1" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-volunteer-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide7" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 bg-transparent text-black hover:text-white hover:bg-gray-900">&lang;</a>
+                        <a href="#slide2" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide2" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-pemilu-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide1" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide3" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide2" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-pemilu-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide3" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide3" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-opening-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide2" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide4" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide3" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-opening-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide4" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide4" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-mubes-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide3" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide5" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide4" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-mubes-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide5" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide5" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-makrab-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide4" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide6" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide5" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-makrab-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide6" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide6" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-libre-1.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide5" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide7" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide6" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-libre-1.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide5" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide7" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide7" class="carousel-item relative w-full">
-                <img
-                src="{{ asset('images/g-mubes-2.jpg') }}"
-                class="w-full" />
-                <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide6" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
-                    <a href="#slide1" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                <div id="slide7" class="carousel-item relative w-full">
+                    <img
+                    src="{{ asset('images/g-mubes-2.jpg') }}"
+                    class="w-full" />
+                    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide6" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&lang;</a>
+                        <a href="#slide1" class="btn btn-circle w-8 h-8 lg:w-10 lg:h-10 text-black hover:text-white bg-transparent hover:bg-gray-900">&rang;</a>
+                    </div>
                 </div>
             </div>
         </div>
