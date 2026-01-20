0c5c8d54da7076ee7159aca2ee6551cf915d10e2
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 08:17:07 +0700

    adding max w for about us

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,9 +1,9 @@
-<div class="flex mx-auto h-fit max-h-screen bg-white bg-about-us-mobile md:bg-about-us bg-cover bg-center justify-items-center items-center px-6 py-12 hidden-overflow">
-    <div class="mx-auto">
-        <div>
-            <img src="{{ asset('images/logo-fortik-3b.png') }}" alt="Logo Fortik" loading="lazy" class="w-[200px] h-[65px] md:w-[375px] md:h-[117px] lg:w-[500px] lg:h-[156px] mx-auto mb-4">
-            <h2 class="font-semibold text-lg text-center italic underline text-koamaru mb-6 md:text-2xl lg:text-4xl">#Develop Your Creativity</h2>
-            <p class="font-normal text-sm text-justify text-koamaru md:text-lg lg:text-xl leading-relaxed md:leading-loose max-w-4xl">
+<div class="flex mx-auto h-fit max-h-screen bg-white bg-about-us-mobile md:bg-about-us bg-cover bg-center justify-items-center items-center px-6 py-12 md:py-20 lg:px-36 hidden-overflow">
+    <div class="w-full max-w-7xl mx-auto">
+        <div class="">
+            <img src="{{ asset('images/logo-fortik-3b.png') }}" alt="Logo Fortik" loading="lazy" class="w-[200px] h-[65px] md:w-[375px] md:h-[117px] lg:w-[500px] lg:h-[156px] mx-auto mb-4 lg:mb-8">
+            <h2 class="font-semibold text-lg text-center italic underline text-koamaru mb-6 lg:mb-12 md:text-2xl lg:text-4xl">#Develop Your Creativity</h2>
+            <p class="font-normal text-sm text-center text-koamaru md:text-lg lg:text-xl leading-relaxed max-w-4xl mx-auto">
                 FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur.
             </p>
         </div>
