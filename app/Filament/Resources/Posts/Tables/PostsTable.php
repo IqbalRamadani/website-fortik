7ea12babc9790f4a38af5c2e9bdbf5a6efd28f39
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-20 20:26:33 +0700

    adding table post

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use Filament\Tables\Table;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
-use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 
 class PostsTable
 {
@@ -13,13 +16,23 @@ class PostsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')
+                    ->searchable()
+                    ->weight('bold')
+                    ->limit(50),
+                TextColumn::make('published_at')
+                    ->date('d F Y')
+                    ->sortable(),
             ])
+            
             ->filters([
                 //
             ])
+            ->defaultSort('created_at', 'desc')
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),   
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
