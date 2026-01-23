4ca230816e4736c381589bcaffb1f907da37c4ec
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-23 08:50:33 +0700

    changing faq template to flowbite

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -1,7 +1,59 @@
 <div class="px-6 py-12 md:py-16 lg:py-36 bg-white bg-faq-mobile md:bg-faq bg-cover bg-top border-none">
     <div class="w-full max-w-6xl mx-auto">
         <h2 class="text-3xl md:text-4xl lg:text-6xl font-bold text-center text-koamaru -mt-8 md:-mt-12 lg:-mt-24 mb-8 md:mb-12 lg:mb-24">FAQ</h2>
-        <div class="w-full md:max-w-xl lg:max-w-4xl">
+        
+        <div id="accordion-card" data-accordion="collapse" class="w-full md:max-w-4xl">
+            {{-- FAQ Item 1 --}}
+            <h2 id="accordion-card-heading-1">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white/90 bg-koamaru/90 shadow-lg border border-default hover:text-white hover:bg-koamaru focus:text-white focus:bg-koamaru gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#accordion-card-body-1" aria-expanded="false" aria-controls="accordion-card-body-1">
+                <span class="px-4 text-left">Apa itu FORTIK?</span>
+                <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/></svg>
+                </button>
+            </h2>
+            <div id="accordion-card-body-1" class="hidden border border-t-0 border-default bg-white shadow-lg" aria-labelledby="accordion-card-heading-1">
+                <div class="p-4 md:p-5">
+                    <p class="mb-2 text-body px-4">FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur.</p>
+                </div>
+            </div>
+            {{-- FAQ Item 2 --}}
+            <h2 id="accordion-card-heading-2" class="mt-4">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white/90 bg-koamaru/90 shadow-lg border border-default hover:text-white hover:bg-koamaru focus:text-white focus:bg-koamaru gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#accordion-card-body-2" aria-expanded="false" aria-controls="accordion-card-body-2">
+                <span class="px-4 text-left">Kapan FORTIK membuka pendaftaran anggota baru?</span>
+                <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/></svg>
+                </button>
+            </h2>
+            <div id="accordion-card-body-2" class="hidden border border-t-0 border-default bg-white shadow-lg" aria-labelledby="accordion-card-heading-2">
+                <div class="p-4 md:p-5">
+                    <p class="mb-2 text-body px-4">Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!</p>
+                </div>
+            </div>
+            {{-- FAQ Item 3 --}}
+            <h2 id="accordion-card-heading-3" class="mt-4">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white/90 bg-koamaru/90 shadow-lg border border-default hover:text-white hover:bg-koamaru focus:text-white focus:bg-koamaru gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#accordion-card-body-3" aria-expanded="false" aria-controls="accordion-card-body-3">
+                <span class="px-4 text-left">FORTIK itu ngapain sih?</span>
+                <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/></svg>
+                </button>
+            </h2>
+            <div id="accordion-card-body-3" class="hidden border border-t-0 border-default bg-white shadow-lg" aria-labelledby="accordion-card-heading-3">
+                <div class="p-4 md:p-5">
+                    <p class="mb-2 text-body px-4">FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.</p>
+                </div>
+            </div>
+            {{-- FAQ Item 4 --}}
+            <h2 id="accordion-card-heading-4" class="mt-4">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white/90 bg-koamaru/90 shadow-lg border border-default hover:text-white hover:bg-koamaru focus:text-white focus:bg-koamaru gap-3 [&[aria-expanded='true']]:rounded-b-none [&[aria-expanded='true']]:shadow-none" data-accordion-target="#accordion-card-body-4" aria-expanded="false" aria-controls="accordion-card-body-4">
+                <span class="px-4 text-left">Apa keuntungan masuk FORTIK?</span>
+                <svg data-accordion-icon class="w-5 h-5 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/></svg>
+                </button>
+            </h2>
+            <div id="accordion-card-body-4" class="hidden border border-t-0 border-default bg-white shadow-lg" aria-labelledby="accordion-card-heading-4">
+                <div class="p-4 md:p-5">
+                    <p class="mb-2 text-body px-4">Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.</p>
+                </div>
+            </div>
+        </div>
+
+        {{-- <div class="w-full md:max-w-xl lg:max-w-4xl">
             <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
                 <input type="radio" name="my-accordion-2" checked="checked" />
                 <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
@@ -47,6 +99,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
