13b1661443d5ac1f594564b04281b4ea15458372
IqbalRamadani <hakimramadani24@gmail.com>
2026-01-23 08:51:48 +0700

    deleting faq template by daisyui

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -52,53 +52,5 @@
                 </div>
             </div>
         </div>
-
-        {{-- <div class="w-full md:max-w-xl lg:max-w-4xl">
-            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
-                <input type="radio" name="my-accordion-2" checked="checked" />
-                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
-                    <div class="px-6 lg:px-12">
-                        <span class="font-semibold text-sm lg:text-lg text-white">Kapan FORTIK membuka pendaftaran anggota baru?</span>
-                    </div>
-                </div>
-                <div class="collapse-content bg-white">
-                    <div class="mt-2 px-6 lg:px-12 py-2">
-                        <span class="text-sm lg:text-base text-koamaru text-wrap">
-                            Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
-                <input type="radio" name="my-accordion-2" />
-                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
-                    <div class="px-6 lg:px-12">
-                        <span class="font-semibold text-sm lg:text-lg text-white">FORTIK itu ngapain sih?</span>
-                    </div>
-                </div>
-                <div class="collapse-content bg-white">
-                    <div class="mt-2 px-6 lg:px-12 py-2">
-                        <span class="text-sm lg:text-base text-koamaru">
-                            FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="collapse collapse-arrow bg-white rounded-none mb-2 shadow-xl">
-                <input type="radio" name="my-accordion-2" />
-                <div class="collapse-title bg-linear-to-br from-lkoamaru to-koamaru">
-                    <div class="px-6 lg:px-12">
-                        <span class="font-semibold text-sm lg:text-lg text-white">Apa keuntungan masuk FORTIK?</span>
-                    </div>
-                </div>
-                <div class="collapse-content bg-white">
-                    <div class="mt-2 px-6 lg:px-12 py-2">
-                        <span class="text-sm lg:text-base text-koamaru">
-                            Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
