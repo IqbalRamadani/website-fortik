ccd86f9ffa8bdaaaefd793df7d7648ee656091cb
Iqbal Ramadani <hakimramadani24@gmail.com>
2026-02-14 10:54:09 +0700

    Revert "Struktur"

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -10,7 +10,7 @@
         <div class="grid md:grid-cols-2 lg:grid-cols-4 place-items-center gap-8">
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 mx-auto mb-4 rounded-full">
-                    <img src="{{ asset('images/img-0.png') }}" alt="latif" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-0.png') }}" alt="latif" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Ketua FORTIK</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Latif</p>
@@ -18,7 +18,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-2.png') }}" alt="iqbal" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-2.png') }}" alt="iqbal" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Wakil Ketua</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Iqbal Ramadani</p>
@@ -26,7 +26,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-3.png') }}" alt="hasan" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-3.png') }}" alt="hasan" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Sekretaris</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Hasan</p>
@@ -34,7 +34,7 @@
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-4.png') }}" alt="reza" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-4.png') }}" alt="reza" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Bendahara</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Reza Ahmad</p>
@@ -42,7 +42,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-5.png') }}" alt="faruq" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-5.png') }}" alt="faruq" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Desain Grafis</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Faruq Taqiyuddin</p>
@@ -50,7 +50,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-6.png') }}" alt="sinar" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-6.png') }}" alt="sinar" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Videografi</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Sinar Surya</p>
@@ -58,7 +58,7 @@
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-7.png') }}" alt="syadio" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-7.png') }}" alt="syadio" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv Konten Kreator</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Syadio</p>
@@ -66,7 +66,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-8.png') }}" alt="aiman" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-8.png') }}" alt="aiman" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PSDM</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Aiman Abdurrahman</p>
@@ -76,7 +76,7 @@
     
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-9.png') }}" alt="rafif" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-9.png') }}" alt="rafif" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv PR</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Muhammad Rafif</p>
@@ -84,7 +84,7 @@
 
             <div class="size-full border-solid border-4 border-endeavour/95 rounded-4xl shadow-xl p-6 text-center">
                 <div class="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4">
-                    <img src="{{ asset('images/img-10.png') }}" alt="ardan" loading="lazy" class="rounded-full">
+                    <img src="{{ asset('images/img-10.png') }}" alt="ardan" class="rounded-full">
                 </div>
                 <h3 class="text-gray-900 text-xl font-bold mb-2">Kadiv SIIP</h3>
                 <p class="text-gray-800 text-lg font-semibold mb-2">Ardan Omar</p>
