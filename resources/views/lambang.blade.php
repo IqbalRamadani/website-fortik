eb72979bfd63401d56a2125c3ca36cf79bb3f09a
farhansyflu <farhansyflu@gmail.com>
2026-02-14 09:04:34 +0700

    hapus tampilan page lambang

--- resources/views/lambang.blade.php
+++ resources/views/lambang.blade.php
@@ -1,50 +1,6 @@
 <x-layout>
-  <div class="px-6 py-12 md:py-16 lg:py-36 bg-white border-none">
-    <div class="w-full max-w-6xl mx-auto">
-  
-            <section class="mt-16 md:mt-10 lg:mt-0 mb-10 lg:mb-16">
-              <p class="font-bold text-sm md:text-md lg:text-lg">TENTANG > MAKNA LAMBANG</p>
-            </section>
-
-            <section class="mb-16">
-                <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-16">MAKNA LAMBANG</h1>
-                <div class="place-items-center mb-16">
-                  <img src="../images/logo-fortik-1b.png" class="w-40 lg:w-70" alt="Logo-FORTIK">
-                </div>
-                <div class="flex flex-col align-items-center justify-content-center gap-5">
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">1</p>
-                    <p>Lambang FORTIK STDI Imam Syafi’i Jember mengacu pada lambang STDI imam Syafi’i.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start  lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">2</p>
-                    <p>Arah Panah Keatas bermakna Bangkit menjadi kampus yang maju dalam dunial digital dengan meningkatkan pemahaman Mahasiswa di Bidang TIK.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">3</p>
-                    <p>Orang Berkolaborasi mempunyai makna menjadi salah satu UKM yang aktif berkontribusi pada setiap acara yang diadakan kampus dan/atau setiap UKM kampus dalam merealisasikan Program Kerja yang direncanakan.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">4</p>
-                    <p>Buku Terbuka (Edukasi) maknanya adalah fokus utama kami adalam menangani buta teknologi dan meningkatkan keterampilan setiap mahasiswa pada bidang digital.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">5</p>
-                    <p>Huruf “T” yaitu Teknologi yang menjadi fokus utama UKM ini.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">6</p>
-                    <p>Huruf “F” yakni Forum maksudnya adalah menjadi sebuah forum diskusi dan konsultasi yang berkaitan dengan teknologi informasi dan komunikasi.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">7</p>
-                    <p>Jalan yang dimaksud jalan disini adalah Free Access. Maksudnya adalah diantara salah satu fokus kami adalah mengkampanyekan software- software berlisensi FOSS atau Free Open Source Software.</p>
-                  </div>
-                </div>
-                <hr class="w-full h-1 bg-koamaru mt-8"></hr>
-            </section>
-  
-
-    </div>
+  <div class="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-koamaru to-matcha lg:flex-row">
+    <h1 class="font-bold text-2xl md:text-3xl p-8 text-center text-gray-200">Halaman sedang dalam proses pengembangan</h1>
+    <img src="{{ asset('images/img-bot-1.png') }}" alt="bot fortik" class="size-20 md:size-24">
   </div>
 </x-layout>
