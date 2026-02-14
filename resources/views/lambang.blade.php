9e623094fc346247e34d667f8c5e76414462e71e
farhansyflu <farhansyflu@gmail.com>
2026-02-14 09:23:04 +0700

    Merapikan kode

--- resources/views/lambang.blade.php
+++ resources/views/lambang.blade.php
@@ -7,40 +7,33 @@
             </section>
 
             <section class="mb-16">
-                <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-16">MAKNA LAMBANG</h1>
+                <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-16">MAKNA LAMBANG</h1>
+
                 <div class="place-items-center mb-16">
-                  <img src="../images/logo-fortik-1b.png" class="w-40 lg:w-70" alt="Logo-FORTIK">
+                  <img src="../images/logo-fortik-1b.png" class="w-40 lg:w-60" alt="Logo-FORTIK">
                 </div>
+
                 <div class="flex flex-col align-items-center justify-content-center gap-5">
+
+                <?php 
+                $texts = ["Lambang FORTIK STDI Imam Syafi’i Jember mengacu pada lambang STDI imam Syafi’i.","Arah Panah Keatas bermakna Bangkit menjadi kampus yang maju dalam dunia digital dengan meningkatkan pemahaman Mahasiswa di Bidang TIK.",
+                "Orang Berkolaborasi mempunyai makna menjadi salah satu UKM yang aktif berkontribusi pada setiap acara yang diadakan kampus dan/atau setiap UKM kampus dalam merealisasikan Program Kerja yang direncanakan.",
+                "Buku Terbuka (Edukasi) maknanya adalah fokus utama kami dalam menangani buta teknologi dan meningkatkan keterampilan setiap mahasiswa pada bidang digital.",
+                "Huruf “T” yaitu Teknologi yang menjadi fokus utama UKM ini.",
+                "Huruf “F” yakni Forum maksudnya adalah menjadi sebuah forum diskusi dan konsultasi yang berkaitan dengan teknologi informasi dan komunikasi.",
+                "Jalan yang dimaksud jalan disini adalah Free Access. Maksudnya adalah diantara salah satu fokus kami adalah mengkampanyekan software-software berlisensi FOSS atau Free Open Source Software."];
+
+                ?>
+
+                <?php foreach ($texts as $index => $text) : ?>
                   <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">1</p>
-                    <p>Lambang FORTIK STDI Imam Syafi’i Jember mengacu pada lambang STDI imam Syafi’i.</p>
+                    <p class="px-4 py-2 bg-koamaru text-white ">
+                      <?= $index + 1 ?>
+                    </p>
+                    <p><?= $text ?></p>
                   </div>
-                  <div class="flex gap-5 place-items-start  lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">2</p>
-                    <p>Arah Panah Keatas bermakna Bangkit menjadi kampus yang maju dalam dunial digital dengan meningkatkan pemahaman Mahasiswa di Bidang TIK.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">3</p>
-                    <p>Orang Berkolaborasi mempunyai makna menjadi salah satu UKM yang aktif berkontribusi pada setiap acara yang diadakan kampus dan/atau setiap UKM kampus dalam merealisasikan Program Kerja yang direncanakan.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">4</p>
-                    <p>Buku Terbuka (Edukasi) maknanya adalah fokus utama kami adalam menangani buta teknologi dan meningkatkan keterampilan setiap mahasiswa pada bidang digital.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">5</p>
-                    <p>Huruf “T” yaitu Teknologi yang menjadi fokus utama UKM ini.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">6</p>
-                    <p>Huruf “F” yakni Forum maksudnya adalah menjadi sebuah forum diskusi dan konsultasi yang berkaitan dengan teknologi informasi dan komunikasi.</p>
-                  </div>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">7</p>
-                    <p>Jalan yang dimaksud jalan disini adalah Free Access. Maksudnya adalah diantara salah satu fokus kami adalah mengkampanyekan software- software berlisensi FOSS atau Free Open Source Software.</p>
-                  </div>
-                </div>
+                <?php endforeach ?>
+
                 <hr class="w-full h-1 bg-koamaru mt-8"></hr>
             </section>
   
