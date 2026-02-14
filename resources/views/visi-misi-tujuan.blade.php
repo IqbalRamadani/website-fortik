2c06eec01727e800bbb1501604a1165f79c917f0
farhansyflu <farhansyflu@gmail.com>
2026-02-14 08:53:39 +0700

    penyesuaian ukuran font

--- resources/views/visi-misi-tujuan.blade.php
+++ resources/views/visi-misi-tujuan.blade.php
@@ -4,12 +4,12 @@
 
             <!-- BREADCRUMB Section -->
             <section class="mt-16 md:mt-10 lg:mt-0 mb-10 lg:mb-16">
-              <p class="font-bold text-sm md:text-lg lg:text-xl">TENTANG > VISI, MISI DAN TUJUAN</p>
+              <p class="font-bold text-sm md:text-md lg:text-lg">TENTANG > VISI, MISI DAN TUJUAN</p>
             </section>
 
             <!-- VISI Section -->
             <section class="mb-16">
-                <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-koamaru mb-6">VISI</h1>
+                <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-6">VISI</h1>
                 <p class="text-sm md:text-lg lg:text-xl leading-relaxed text-justify">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                 </p>
@@ -18,7 +18,7 @@
 
             <!-- MISI Section -->
             <section class="mb-16">
-                <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold text-koamaru mb-6">MISI</h2>
+                <h2 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-6">MISI</h2>
                 <ol class="text-sm md:text-lg lg:text-xl list-decimal list-inside space-y-2">
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
                     <li>Quisque eu libero sit amet eros lobortis faucibus ut vitae justo.</li>
@@ -31,7 +31,7 @@
 
             <!-- TUJUAN Section -->
             <section class="mb-16">
-                <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold text-koamaru mb-6">TUJUAN</h2>
+                <h2 class="text-3xl md:text-4xl lg:text-5xl font-bold text-koamaru mb-6">TUJUAN</h2>
                 <ol class="text-sm md:text-lg lg:text-xl list-decimal list-inside space-y-2">
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
                     <li>Quisque eu libero sit amet eros lobortis faucibus ut vitae justo.</li>
