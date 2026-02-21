7c03c3a1b71fe52c4f1100b927766e3c51dcbf0e
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-21 10:15:59 +0700

    renaming to Post

--- app/Models/post.php
+++ app/Models/post.php
@@ -6,30 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class post extends Model
+class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
     protected $fillable = [
-        'title', 
-        'slug', 
-        'user_id', 
-        'published_at', 
-        'content', 
+        'title',
+        'slug',
+        'user_id',
+        'published_at',
+        'content',
         'image',
     ];
 
     protected $casts = [
         'published_at' => 'date',
     ];
-
-    protected $with = ['author'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 
     public function author(): BelongsTo
     {
