9280416b498ce289db969a3abd41d03ab0904b05
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 06:52:54 +0700

    edit model

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    protected $guarded = [];
+    public function album() {
+        return $this->belongsTo(Album::class);
+    }
 }
