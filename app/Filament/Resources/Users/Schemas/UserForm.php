29eacb76e6cf693a002b49acacfff10658d1d75a
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-21 16:50:23 +0700

    create filament user resource

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                ->label('Nama Lengkap')
+                ->required()
+                ->maxLength(255),
+                TextInput::make('email')
+                ->label('Alamat Email')
+                ->email()
+                ->required()
+                ->unique(ignoreRecord: true)
+                ->maxLength(255),
+                TextInput::make('password')
+                ->label('Password')
+                ->password()
+                ->revealable()
+                ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                ->dehydrated(fn ($state) => filled($state))
+                ->required(fn ($livewire) => $livewire instanceof CreateRecord)
+                ->maxLength(255),
+            ]);
+    }
+}
