be4aeb35ea666fc6c22705b4d46b6c88a8f724c0
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-21 10:15:39 +0700

    renaming to Bulletin

--- app/Models/bulletin.php
+++ app/Models/bulletin.php
@@ -2,32 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class bulletin extends Model
+class Bulletin extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    /** @use HasFactory<\Database\Factories\BulletinFactory> */
     use HasFactory;
 
     protected $fillable = [
-        'title', 
-        'slug', 
-        'user_id', 
-        'published_at', 
-        'content', 
+        'title',
+        'slug',
+        'user_id',
+        'published_at',
+        'content',
         'image',
     ];
 
     protected $casts = [
         'published_at' => 'date',
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 
     public function author(): BelongsTo
     {
