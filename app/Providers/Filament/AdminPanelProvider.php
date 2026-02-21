07eec9272b5db99403a93dfc3a1bb4c5e54b0e1d
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-21 10:14:07 +0700

    adding favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->favicon(asset('images/logo-panel.png'))
             ->sidebarCollapsibleOnDesktop()
                 ->brandLogo(asset('images/logo-panel.png'))
                 ->brandLogoHeight('2rem')
