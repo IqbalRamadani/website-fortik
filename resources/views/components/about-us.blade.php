7db2ba7bf02b092d005527043d89603258b037f7
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 17:47:55 +0700

    change style about us

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,11 +1,15 @@
 <div class="flex mx-auto h-fit max-h-screen bg-white bg-about-us-mobile md:bg-about-us bg-cover bg-center justify-items-center items-center px-6 py-12 md:py-20 lg:px-36 hidden-overflow">
     <div class="w-full max-w-6xl mx-auto">
         <div class="">
-            <img src="{{ asset('images/logo-panel.png') }}" alt="Logo Fortik" loading="lazy" class="w-80 md:w-140 mx-auto mb-4 lg:mb-8">
-            <h2 class="font-semibold text-lg text-center italic underline text-koamaru mb-6 lg:mb-12 md:text-2xl lg:text-4xl">#Develop Your Creativity</h2>
-            <p class="font-normal text-sm text-center text-koamaru md:text-lg lg:text-xl leading-relaxed max-w-4xl mx-auto">
+            {{-- <img src="{{ asset('images/logo-panel.png') }}" alt="Logo Fortik" loading="lazy" class="w-80 md:w-140 mx-auto mb-4 lg:mb-8"> --}}
+            <h2 class="font-extrabold text-5xl md:text-7xl text-center underline text-koamaru mb-8">KABINET</h2>
+            <img src="{{ asset('images/logo-kabinet.png') }}" alt="logo kabinet" loading="lazy" class="w-80 md:w-140 mx-auto mb-8">
+            <div class="flex justify-center ml-4">
+                <span class="p-4 bg-koamaru rounded-lg text-center text-white font-bold text-xl md:text-3xl lg:text-4xl">Cerdas <span class="font-light">dalam</span> Berinovasi</span>
+            </div>
+            {{-- <p class="font-normal text-sm text-center text-koamaru md:text-lg lg:text-xl leading-relaxed max-w-4xl mx-auto">
                 FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur.
-            </p>
+            </p> --}}
         </div>
     </div>
 </div>
