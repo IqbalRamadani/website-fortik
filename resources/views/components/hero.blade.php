ce8251205d006053b7b8c2d1015ddbb213398596
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 16:40:06 +0700

    editing hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,19 +1,22 @@
 <div class="flex mx-auto min-h-screen bg-hero bg-cover bg-center justify-items-center items-center px-6 py-8 md:px-16 lg:py-8 ">
-    <div class="w-full max-w-6xl mx-auto"></div>
-    {{-- <div class="mt-16 sm:mt-12 py-6 lg:m-auto lg:col-span-4">
-        <img src="{{ asset('images/logo-fortik-1w.png') }}" alt="Logo Fortik" loading="lazy" class="w-[145px] h-[200px] lg:w-[203px] lg:h-[280px]">
+    <div class="w-full max-w-6xl mx-auto">
+        <div class="grid grid-cols-1 lg:grid-cols-12 gap-8">
+            <div class="mt-16 sm:mt-12 py-6 lg:m-auto lg:col-span-5">
+                <img src="{{ asset('images/logo-fortik-1w.webp') }}" alt="Logo Fortik" class="w-30 md:w-40 mx-auto">
+            </div>
+            <div class="mx-auto mb-12 md:-mb-8 text-center lg:col-span-7">
+                <h1 class="mb-4 text-2xl font-extrabold text-white md:text-3xl lg:text-5xl leading-tight">Forum Teknologi Informasi dan Komunikasi</h1>
+                <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDI&rpar; Jember, Jawa Timur.</p>
+                <a href="https://t.me/kanalfortikstdiis" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:bg-lkoamaru shadow-lg focus:ring-4 focus:ring-primary-300">
+                    Gabung Kanal Telegram
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram ml-2" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                    </svg>
+                </a>
+                {{-- <a href="/about" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:bg-lkoamaru focus:ring-4 focus:ring-gray-100">
+                    Baca Selengkapnya
+                </a>  --}}
+            </div>
+        </div>
     </div>
-    <div class="mx-auto mb-12 md:mb-4 text-center lg:col-span-8">
-        <h1 class="max-w-3xl mb-4 text-2xl font-extrabold tracking-tight leading-none text-white md:text-5xl xl:text-6xl">Forum Teknologi Informasi dan Komunikasi</h1>
-        <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDI&rpar; Jember - Jawa Timur.</p>
-        <a href="https://t.me/kanalfortikstdiis" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-primary-800 hover:bg-primary-900 focus:ring-4 focus:ring-primary-300">
-            Gabung
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram ml-2" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
-            </svg>
-        </a>
-        <a href="/about" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-            Baca Selengkapnya
-        </a> 
-    </div>  --}}
 </div>
