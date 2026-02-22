cee8618345375d8b37f2c5541ea92b6f2fc8bc5e
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 10:37:06 +0700

    menambah perintah error jika tidak pakai eager loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Baris ini akan membuat aplikasimu error di lokal jika ada N+1 query, 
+        // tapi akan berjalan normal (mengabaikan error) saat di server IT Kampus.
+        Model::preventLazyLoading(! app()->isProduction());
     }
 }
