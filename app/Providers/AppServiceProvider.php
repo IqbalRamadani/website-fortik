69e17eb685f0484142dba44287022873356ce059
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 20:40:36 +0700

    adding config to pulse open to guest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Baris ini akan membuat aplikasimu error di lokal jika ada N+1 query, 
-        // tapi akan berjalan normal (mengabaikan error) saat di server IT Kampus.
         Model::preventLazyLoading(! app()->isProduction());
+
+        Gate::define('viewPulse', function (?User $user) {
+            return app()->environment('local');
+        });
     }
 }
