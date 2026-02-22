4899c454878a6ec1404496231bbd80eeffafdbee
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-22 07:22:21 +0700

    initialize albumshow livewire

---
+++ app/Livewire/AlbumShow.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Album;
+use Illuminate\Support\Facades\Cache;
+use Livewire\Component;
+
+class AlbumShow extends Component
+{
+    public $slug;
+
+    public function mount($slug) {
+        $this->slug = $slug;
+    }
+
+    public function render()
+    {
+        $album = Cache::remember("album.{$this->slug}", 86400, function () {
+            return Album::with('photos')
+            ->where('slug', $this->slug)
+            ->firstOrFail();
+        });
+
+        return view('livewire.album-show', compact('album'))
+            ->layout('layouts.app');
+    }
+}
