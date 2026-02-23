5f1bd18cca2d3920c42c0317a756de39b8af9e4d
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-23 08:47:01 +0700

    adding scheme

--- database/migrations/2026_02_23_082637_create_divisions_table.php
+++ database/migrations/2026_02_23_082637_create_divisions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('divisions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('sort_order')->default(0);
             $table->timestamps();
         });
     }
