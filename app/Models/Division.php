3cc98eec0e6717657f399c0a599497a24fa20947
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-23 08:48:43 +0700

    add logic model

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Division extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function members() {
+        return $this->hasMany(Member::class)->orderBy('sort_order');
+    }
+
+    protected static function booted() {
+        static::saved(fn () => Cache::forget('struktur-organisasi'));
+        static::deleted(fn () => Cache::forget('struktur-organisasi'));
+    }
 }
