91c2c7e532e0f3848ef2af6f772a0903933d8cdc
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-23 08:49:26 +0700

    add logic model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 
 class Member extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function division() {
+        return $this->belongsTo(Division::class);
+    }
+
+    protected static function booted() {
+        static::saved(fn () => Cache::forget('struktur-organisasi'));
+        static::deleted(function ($member) {
+            Cache::forget('struktur-organisasi');
+            if ($member->image && Storage::disk('public')->exists($member->image)) {
+                Storage::disk('public')->delete($member->image);
+            }
+        });
+    }
 }
