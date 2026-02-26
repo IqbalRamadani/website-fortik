5c2f4de7753ff0c1a823dd1ef77471ce949a5848
farhansyflu <farhansyflu@gmail.com>
2026-02-26 13:52:15 +0700

    buat tampilan page lambang

--- resources/views/lambang.blade.php
+++ resources/views/lambang.blade.php
@@ -1,43 +1,75 @@
 <x-layout>
   <div class="px-6 py-12 md:py-16 lg:py-36 bg-white border-none">
     <div class="w-full max-w-6xl mx-auto">
-  
-            <section class="mt-16 md:mt-10 lg:mt-0 mb-10 lg:mb-16">
-              <p class="font-bold text-sm md:text-md lg:text-lg">TENTANG > MAKNA LAMBANG</p>
-            </section>
+      
+      <section>
+          <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-24">MAKNA LAMBANG</h1>
 
-            <section class="mb-16">
-                <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-24">MAKNA LAMBANG</h1>
+          <div class="place-items-center mb-16">
+              <img src="images/logo-fortik-1b.png" class="w-40 lg:w-60" alt="Logo-FORTIK">
+          </div>
 
-                <div class="place-items-center mb-16">
-                  <img src="images/logo-fortik-1b.png" class="w-40 lg:w-60" alt="Logo-FORTIK">
-                </div>
+          <div class="mb-32">
+            <h1 class="text-koamaru">Perkembangan zaman yang disertai dengan kemajuan teknologinya menuntut kita untuk lebih aktif dalam mengelola dan mengolah teknologi dengan sebaik mungkin. Hal inilah yang menjadi perhatian FORTIK untuk dapat meningkatkan serta mengembangkan keterampilan setiap mahasiswa dalam bidang digital, dengan berbagai pelayanan yang ditawarkan mulai dari bentuk kerjasama antar UKM hingga pelayanan pendidikan yang dapat diakses oleh setiap mahasiswanya. FORTIK hadir untuk dapat membangkitkan semangat mahasiswa dalam berteknologi serta menciptakan kampus yang maju dalam dunia digital.</h1>
+          </div>
 
-                <div class="flex flex-col align-items-center justify-content-center gap-5">
+          <div class="flex flex-col align-items-center justify-content-center gap-5">
 
-                <?php 
-                $texts = ["Lambang FORTIK STDI Imam Syafi’i Jember mengacu pada lambang STDI imam Syafi’i.","Arah Panah Keatas bermakna Bangkit menjadi kampus yang maju dalam dunia digital dengan meningkatkan pemahaman Mahasiswa di Bidang TIK.",
-                "Orang Berkolaborasi mempunyai makna menjadi salah satu UKM yang aktif berkontribusi pada setiap acara yang diadakan kampus dan/atau setiap UKM kampus dalam merealisasikan Program Kerja yang direncanakan.",
-                "Buku Terbuka (Edukasi) maknanya adalah fokus utama kami dalam menangani buta teknologi dan meningkatkan keterampilan setiap mahasiswa pada bidang digital.",
-                "Huruf “T” yaitu Teknologi yang menjadi fokus utama UKM ini.",
-                "Huruf “F” yakni Forum maksudnya adalah menjadi sebuah forum diskusi dan konsultasi yang berkaitan dengan teknologi informasi dan komunikasi.",
-                "Jalan yang dimaksud jalan disini adalah Free Access. Maksudnya adalah diantara salah satu fokus kami adalah mengkampanyekan software-software berlisensi FOSS atau Free Open Source Software."];
+                      <?php 
+                      $details = [
+                          [
+                              "judul" => "Arah panah keatas",
+                              "deskripsi" => "Bangkit menjadi kampus yang maju dalam dunia digital dengan meningkatkan pemahaman mahasiswa dalam mengelola dan mengolah teknologi.",
+                              "gambar" => "images/panah-keatas.webp"
+                          ],
+                          [
+                              "judul" => "Orang (Kolaborasi)",
+                              "deskripsi" => "Menjadi salah satu Unit Kegiatan Mahasiswa yang aktif berkontribusi pada setiap acara yang diadakan oleh kampus, serta membantu setiap UKM kampus dalam merealisasikan Program kerja yang direncanakan.",
+                              "gambar" => "images/orang-fortik.webp"
+                          ],
+                          [
+                              "judul" => "Buku (Edukasi)",
+                              "deskripsi" => "Menjadi salah satu fokus utama dalam menangani buta teknologi dan meningkatkan keterampilan setiap mahasiswa pada bidang digital.",
+                              "gambar" => "images/buku-fortik.webp"
+                          ],
+                          [
+                              "judul" => 'Huruf "T" (Teknologi)',
+                              "deskripsi" => "Teknologi merupakan fokus utama FORTIK.",
+                              "gambar" => "images/huruf-t.webp"
+                          ],
+                          [
+                              "judul" => 'Huruf "F" (Forum)',
+                              "deskripsi" => "Menjadi sebuah forum diskusi dan konsultasi yang berkaitan dengan teknologi, informasi dan komunikasi.",
+                              "gambar" => "images/huruf-f.webp"
+                          ],
+                          [
+                              "judul" => "Jalan (Free Access)",
+                              "deskripsi" => "Menjadi salah satu fokus utama kami; bahwa software-software yang kami kampanyekan berlisensi FOSS (Free Open Source Software).",
+                              "gambar" => "images/free-access.webp"
+                          ],
+                      ];
+                      ?>
 
-                ?>
+                          <div class="grid md:grid-cols-3 gap-20 relative">
+                                  <?php foreach ($details as $index => $detail): ?>
+                                      <div class="text-center p-3 lg:p-6">
 
-                <?php foreach ($texts as $index => $text) : ?>
-                  <div class="flex gap-5 place-items-start lg:place-items-center">
-                    <p class="px-4 py-2 bg-koamaru text-white ">
-                      <?= $index + 1 ?>
-                    </p>
-                    <p class="text-xl"><?= $text ?></p>
-                  </div>
-                <?php endforeach ?>
+                                          <div class="flex justify-center mb-6">
+                                              <img src=<?= $detail["gambar"] ?> alt="" class="w-35">
+                                          </div>
 
-                <hr class="w-full h-1 bg-koamaru mt-8"></hr>
-            </section>
-  
+                                          <h3 class="text-lg font-bold text-blue-800 mb-3">
+                                              <?= $detail['judul'] ?>
+                                          </h3>
 
-    </div>
+                                          <p class="text-koamaru leading-relaxed">
+                                              <?= $detail['deskripsi'] ?>
+                                          </p>
+                                      </div>
+
+                                  <?php endforeach; ?>
+                              </div>
+                  </section>
+          </div>
   </div>
 </x-layout>
