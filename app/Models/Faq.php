235874c442f94113bbd3193d9331cb2aea255bfb
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-26 13:49:24 +0700

    create model faq

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Faq extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected static function booted() {
+        static::saved(fn () => Cache::forget('faqs_data'));
+        static::deleted(fn () => Cache::forget('faqs_data'));
+    }
 }
