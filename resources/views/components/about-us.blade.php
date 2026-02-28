3c4e24eab3ef8dcec3f6c78a22c7d2fcea8b3a36
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-28 13:07:27 +0700

    changing content to logo

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,15 +1,19 @@
-<div class="flex mx-auto h-fit max-h-screen bg-white bg-about-us-mobile md:bg-about-us bg-cover bg-center justify-items-center items-center px-6 py-12 md:py-20 lg:px-36 hidden-overflow">
-    <div class="w-full max-w-6xl mx-auto">
-        <div class="">
-            {{-- <img src="{{ asset('images/logo-panel.png') }}" alt="Logo Fortik" loading="lazy" class="w-80 md:w-140 mx-auto mb-4 lg:mb-8"> --}}
+<div class="flex mx-auto h-fit max-h-screen bg-slate-100 justify-items-center items-center px-6 py-8 lg:px-36 hidden-overflow">
+    <div class="w-full max-w-4xl mx-auto">
+        <div class="flex flex-row justify-center items-center">
+            <img src="{{ asset('images/logo-fortik-3b.webp') }}" alt="Logo Fortik" loading="lazy" class="w-40 md:w-80 mx-auto">
+            <img src="{{ asset('images/logo-kabinet.png') }}" alt="Logo Kabinet" loading="lazy" class="w-40 md:w-80 mx-auto">
+        </div>
+        {{-- <div class="">
+            <img src="{{ asset('images/logo-panel.png') }}" alt="Logo Fortik" loading="lazy" class="w-80 md:w-140 mx-auto mb-4 lg:mb-8">
             <h2 class="font-extrabold text-5xl md:text-7xl text-center underline text-koamaru mb-8">KABINET</h2>
             <img src="{{ asset('images/logo-kabinet.png') }}" alt="logo kabinet" loading="lazy" class="w-60 md:w-100 lg:w-140 mx-auto mb-8">
             <div class="flex justify-center ml-2">
                 <span class="p-4 bg-linear-to-br from-lkoamaru to-koamaru rounded-lg text-center text-white font-bold text-lg md:text-3xl lg:text-4xl">Cerdas <span class="font-light">dalam</span> Berinovasi</span>
             </div>
-            {{-- <p class="font-normal text-sm text-center text-koamaru md:text-lg lg:text-xl leading-relaxed max-w-4xl mx-auto">
+            <p class="font-normal text-sm text-center text-koamaru md:text-lg lg:text-xl leading-relaxed max-w-4xl mx-auto">
                 FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDIIS&rpar; Jember, Jawa Timur.
-            </p> --}}
-        </div>
+            </p>
+        </div> --}}
     </div>
 </div>
