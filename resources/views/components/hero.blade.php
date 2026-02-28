7710b1a760ffedd9b8fd1cef1eba20d83021a68d
IqbalRamadani <hakimramadani24@gmail.com>
2026-02-28 13:10:43 +0700

    change content to banner

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,28 +1,81 @@
-<div class="flex mx-auto min-h-screen bg-hero bg-cover bg-center justify-items-center items-center px-6 py-8 md:px-16 lg:py-8 ">
-    <div class="w-full max-w-6xl mx-auto">
-        <div class="grid grid-cols-1 lg:grid-cols-12 gap-8">
-            <div class="mt-16 sm:mt-12 py-6 lg:m-auto lg:col-span-5">
-                <img src="{{ asset('images/logo-fortik-1w.webp') }}" alt="Logo Fortik" class="w-30 md:w-40 mx-auto">
-            </div>
-            <div class="mx-auto mb-12 md:-mb-8 text-center lg:col-span-7">
-                <h1 class="mb-4 text-2xl font-extrabold text-white md:text-3xl lg:text-5xl leading-tight">Forum Teknologi Informasi dan Komunikasi</h1>
-                <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDI&rpar; Jember, Jawa Timur.</p>
-                <a href="https://t.me/kanalfortikstdiis" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:text-supernova shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-x-0 hover:scale-105">
-                    Join Telegram
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram ml-2" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
-                    </svg>
-                </a>
-                <a href="https://www.instagram.com/fortik_stdiis?igsh=MTM2cnZ2Y3JvdmE1aQ==" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:text-supernova shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-x-0 hover:scale-105">
-                    Follow Instagram
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram ml-2" viewBox="0 0 16 16">
-                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-                    </svg>
-                </a>
-                {{-- <a href="/about" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:bg-lkoamaru focus:ring-4 focus:ring-gray-100">
-                    Baca Selengkapnya
-                </a>  --}}
-            </div>
+@php
+    $banners = [
+        [
+            'image' => 'images/banner-1.png',
+            'url'   => 'https://fortik.test/fornews/deserunt-et-enim-dolores-ducimus-nostrum-vL9cu',
+        ],
+        [
+            'image' => 'images/banner-2.png',
+            'url'   => 'https://fortik.test/fornews/at-ex-sed-aspernatur-dolorem-voluptatem-oQT4n',
+        ],
+        [
+            'image' => 'images/banner-3.png',
+            'url'   => 'https://fortik.test/fornews/consequatur-temporibus-incidunt-recusandae-autem-illo-tempora-XprmU',
+        ],
+    ];
+@endphp
+
+<div
+    x-data
+    x-init="
+        new Swiper($refs.container, {
+            loop: true,
+            autoplay: { delay: 3000, disableOnInteraction: false },
+            speed: 800,
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: { el: '.swiper-pagination', clickable: true },
+        })
+    "
+    class="relative w-full"
+>
+    <div x-ref="container" class="swiper w-full">
+        <div class="swiper-wrapper">
+
+            @foreach ($banners as $banner)
+                <div class="swiper-slide">
+                    <a href="{{ $banner['url'] }}" class="block w-full h-full">
+                        <img
+                            src="{{ asset($banner['image']) }}"
+                            class="w-screen"
+                            alt="Banner"
+                        >
+                    </a>
+                </div>
+            @endforeach
+
         </div>
+
+        <div class="swiper-button-prev !w-8 !h-8 md:!w-12 md:!h-12 lg:!w-14 lg:!h-14 bg-white/45 hover:bg-gray-900 !text-koamaru hover:!text-white"><span class="text-sm md:text-lg lg:text-xl font-bold ">&lang;</span></div>
+        <div class="swiper-button-next !w-8 !h-8 md:!w-12 md:!h-12 lg:!w-14 lg:!h-14 bg-white/45 hover:bg-gray-900 !text-koamaru hover:!text-white"><span class="text-sm md:text-lg lg:text-xl font-bold ">&rang;</span></div>
+        <div class="swiper-pagination"></div>
     </div>
 </div>
+    {{-- <div class="grid grid-cols-1 lg:grid-cols-12 gap-8">
+        <div class="mt-16 sm:mt-12 py-6 lg:m-auto lg:col-span-5">
+            <img src="{{ asset('images/logo-fortik-1w.webp') }}" alt="Logo Fortik" class="w-30 md:w-40 mx-auto">
+        </div>
+        <div class="mx-auto mb-12 md:-mb-8 text-center lg:col-span-7">
+            <h1 class="mb-4 text-2xl font-extrabold text-white md:text-3xl lg:text-5xl leading-tight">Forum Teknologi Informasi dan Komunikasi</h1>
+            <p class="max-w-3xl mb-6 font-normal text-gray-200 lg:mb-8 text-base md:text-lg">Selamat datang di website resmi Forum Teknologi Informasi dan Komunikasi &lpar;FORTIK&rpar; yang merupakan salah satu Unit Kegiatan Mahasiswa &lpar;UKM&rpar; Sekolah Tinggi Dirasat Islamiyyah Imam Syafi'i &lpar;STDI&rpar; Jember, Jawa Timur.</p>
+            <a href="https://t.me/kanalfortikstdiis" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:text-supernova shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-x-0 hover:scale-105">
+                Join Telegram
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram ml-2" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                </svg>
+            </a>
+            <a href="https://www.instagram.com/fortik_stdiis?igsh=MTM2cnZ2Y3JvdmE1aQ==" target="_blank" class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-4 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:text-supernova shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-x-0 hover:scale-105">
+                Follow Instagram
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram ml-2" viewBox="0 0 16 16">
+                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                </svg>
+            </a>
+            <a href="/about" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-koamaru hover:bg-lkoamaru focus:ring-4 focus:ring-gray-100">
+                Baca Selengkapnya
+            </a> 
+        </div>
+    </div> --}}
+
