dbed28f45cf1cd19ce8c226bfa02ad8b21115923
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-01 15:16:38 +0700

    resize hr n mb

--- resources/views/visi-misi-tujuan.blade.php
+++ resources/views/visi-misi-tujuan.blade.php
@@ -2,17 +2,17 @@
     <div class="px-6 py-12 bg-white border-none">
         <div class="w-full max-w-6xl mx-auto">
             <!-- VISI Section -->
-            <section class="mb-16">
-                <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-6">VISI</h1>
+            <section class="mb-8">
+                <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-4">VISI</h1>
                 <p class="text-sm md:text-lg lg:text-xl leading-relaxed text-justify">
                     Mewujudkan UKM FORTIK STDIIS sebagai pusat informasi teknologi, informasi, dan komunikasi yang kreatif, kolaboratif, dan berdaya saing tinggi, serta menjadi wadah pengembangan softskill dan hardskill bagi seluruh pengurusnya.
                 </p>
-                <hr class="w-full h-1 bg-koamaru mt-8"></hr>
+                <hr class="w-full h-px bg-koamaru mt-8"></hr>
             </section>
 
             <!-- MISI Section -->
-            <section class="mb-16">
-                <h2 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-6">MISI</h2>
+            <section class="mb-8">
+                <h2 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-4">MISI</h2>
                 <ol class="text-sm md:text-lg lg:text-xl list-decimal list-outside space-y-2 ml-6">
                     <li>Menjadi wadah untuk mengembangkan potensi dan minat secara terbuka bagi pengurus UKM FORTIK STDIIS atau lembaga lainnya.</li>
                     <li>Memfasilitasi serta membangun lingkungan kolaboratif dengan pihak internal maupun eksternal bagi pengurus UKM FORTIK STDIIS.</li>
@@ -20,12 +20,12 @@
                     <li>Memberikan partisipasi bermanfaat dalam bentuk relawan bagi kampus dan masyarakat.</li>
                     <li>Mengkaji ulang kegiatan-kegiatan yang diadakan pengurus UKM FORTIK STDIIS dengan cermat dan terstruktur.</li>
                 </ol>
-                <hr class="w-full h-1 bg-koamaru mt-8"></hr>
+                <hr class="w-full h-px bg-koamaru mt-8"></hr>
             </section>
 
             <!-- TUJUAN Section -->
-            <section class="mb-16">
-                <h2 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-6">TUJUAN</h2>
+            <section class="mb-8">
+                <h2 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-koamaru mb-4">TUJUAN</h2>
                     <p class="text-sm md:text-lg lg:text-xl leading-relaxed text-justify">
                             Membentuk akademisi Islam yang sadar dan paham tentang teknologi, informasi, dan komunikasi dalam rangka mencapai visi dan misi STDI Imam Syafi&apos;i Jember dan tujuan agama Islam.
                     </p>
