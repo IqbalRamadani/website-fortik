b7f1a5a0ad6ae21dc3c8dbb9224c5437d0c019f9
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-01 10:27:38 +0700

    add font awesome

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
         <title>{{ $title ?? 'FORTIK - Forum Teknologi Informasi dan Komunikasi' }}</title>
         @stack('meta')
         
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWixF..." crossorigin="anonymous" referrerpolicy="no-referrer"/>
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo-favicon.webp') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
         <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
