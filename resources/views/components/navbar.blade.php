24ddd56781f05db372e60d67df37f2bdc5449d12
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-01 10:29:46 +0700

    hamburger appear in md

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav x-data="{ mobileMenuOpen: false }" class="bg-linear-to-br from-lkoamaru to-koamaru sticky w-full z-20 top-0 start-0 shadow-lg px-6 py-4">
     <div class="flex max-w-2xl md:max-w-3xl lg:max-w-6xl flex-wrap items-center justify-between mx-auto">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="{{ asset('images/logo-navbar.webp') }}" class="w-30 md:w-40" alt="Logo Fortik"/>
+            <img src="{{ asset('images/logo-navbar.webp') }}" class="w-30 md:w-35" alt="Logo Fortik"/>
         </a>
         <div>
-            <button @click.stop="mobileMenuOpen = !mobileMenuOpen" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg cursor-pointer md:hidden hover:bg-supernova hover:text-koamaru" aria-controls="navbar-dropdown" aria-expanded="false">
+            <button @click.stop="mobileMenuOpen = !mobileMenuOpen" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg cursor-pointer lg:hidden hover:bg-supernova hover:text-koamaru" aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -12,8 +12,8 @@
             </button>
         </div>
 
-        <div :class="{'hidden': !mobileMenuOpen, 'block': mobileMenuOpen}" @click.outside="mobileMenuOpen = false" class="hidden w-full md:block md:w-auto">
-            <ul class="flex flex-col font-semibold text-sm lg:text-base p-0 mt-4 bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+        <div :class="{'hidden': !mobileMenuOpen, 'block': mobileMenuOpen}" @click.outside="mobileMenuOpen = false" class="hidden w-full lg:block lg:w-auto">
+            <ul class="flex flex-col font-semibold text-sm lg:text-base p-0 mt-4 bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-4 lg:mt-0 md:border-0">
                 <li>
                     <x-nav-link href="/" :active="request()->is('/')">BERANDA</x-nav-link>
                 </li>
