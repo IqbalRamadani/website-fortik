bcfa3ce28b260b574a19fa7500b7d3f0468d1331
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-01 15:19:22 +0700

    resize img width n bg-neutral

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,8 +1,8 @@
-<div class="flex mx-auto h-fit max-h-screen bg-neutral-200 justify-items-center items-center px-6 py-8 lg:px-36 hidden-overflow">
+<div class="flex mx-auto h-full max-h-screen bg-neutral-200 justify-items-center items-center px-6 py-4 md:p-8 lg:px-36 hidden-overflow">
     <div class="w-full max-w-4xl mx-auto">
         <div class="flex flex-row justify-center items-center">
-            <img src="{{ asset('images/logo-fortik-3b.webp') }}" alt="Logo Fortik" loading="lazy" class="w-40 md:w-80 mx-auto">
-            <img src="{{ asset('images/logo-kabinet.png') }}" alt="Logo Kabinet" loading="lazy" class="w-40 md:w-80 mx-auto">
+            <img src="{{ asset('images/logo-fortik-3b.webp') }}" alt="Logo Fortik" loading="lazy" class="w-24 md:w-48 lg:w-80 mx-auto">
+            <img src="{{ asset('images/logo-kabinet.png') }}" alt="Logo Kabinet" loading="lazy" class="w-24 md:w-48 lg:w-80 mx-auto">
         </div>
     </div>
 </div>
