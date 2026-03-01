dbdf12687ab289f8f4ba9ac172cb0cc75daa2259
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-02 00:08:28 +0700

    update faq seeder

--- database/seeders/FaqSeeder.php
+++ database/seeders/FaqSeeder.php
@@ -14,20 +14,20 @@ class FaqSeeder extends Seeder
     {
         $items = [
             [
-                'question' => 'Apa itu FORTIK?',
-                'answer' => 'FORTIK adalah singkatan dari Forum Teknologi Informasi dan Komunikasi. Forum ini dibentuk untuk menjadi wadah mahasiswa yang memiliki minat dan bakat di bidang teknologi. FORTIK berdiri pada tanggal 10 Oktober 2022 di Sekolah Tinggi Dirasat Islamiyyah Imam Syafi\'i (STDIIS) Jember, Jawa Timur.'
+                'question' => 'Apa itu FORTIK dan apa fokus utamanya?',
+                'answer' => 'FORTIK adalah Unit Kegiatan Mahasiswa (UKM) yang berfokus pada pengembangan teknologi, namun juga mencakup pengembangan sumber daya manusia dan komunikasi publik melalui berbagai divisi internal.'
             ],
             [
-                'question' => 'Kapan FORTIK membuka pendaftaran anggota baru?',
-                'answer' => 'Kabar gembira nih! FORTIK Insya Allah bakal buka pendaftaran tanggal 1-7 September!! Siap siap yaa!'
+                'question' => 'Kapan pendaftaran anggota baru (Open Recruitment) dibuka?',
+                'answer' => 'Open recruitment diadakan secara rutin setiap awal semester ganjil, tepatnya beberapa hari setelah hari pertama perkuliahan dimulai.'
             ],
             [
-                'question' => 'FORTIK itu ngapain sih?',
-                'answer' => 'FORTIK adalah UKM yang berfokus pada teknologi informasi, kerjaannya? tentu berkaitan dengan Teknologi Informasi dong.'
+                'question' => 'Apa saja divisi yang ada di dalam FORTIK?',
+                'answer' => 'Terdapat enam divisi utama: Desain Grafis, Videografi, Content Creator, SIIP (Sistem Informasi Infrastruktur dan Programming), PSDM (Pengembangan Sumber Daya Manusia) dan Public Relation.'
             ],
             [
-                'question' => 'Apa keuntungan masuk FORTIK?',
-                'answer' => 'Kamu bakal dapet relasi yang solid, pengalaman berorganisasi, skill yang terarah dan terasah, dan tentunya SAKTIFA dong.'
+                'question' => 'Apa acara terbesar yang diselenggarakan oleh FORTIK?',
+                'answer' => 'Acara puncak kami adalah Fortik\'s Fair, sebuah festival teknologi berskala nasional yang mencakup perlombaan, pelatihan, dan webinar nasional.'
             ],
         ];
 
