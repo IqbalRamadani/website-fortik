d532010aa27112a9587c1cce1dcddc85a0a6115a
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-01 20:33:25 +0700

    delete unused link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full scroll-smooth">
+<html lang="id" class="h-full scroll-smooth">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>{{ $title ?? 'FORTIK - Forum Teknologi Informasi dan Komunikasi' }}</title>
         @stack('meta')
-        
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWixF..." crossorigin="anonymous" referrerpolicy="no-referrer"/>
+        {{-- favicon --}}
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo-favicon.webp') }}">
+        {{-- font awesome --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWixF..." crossorigin="anonymous" referrerpolicy="no-referrer"/>
+        {{-- swiper --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-        <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
         @vite('resources/css/app.css')
         @livewireStyles
         <style>
