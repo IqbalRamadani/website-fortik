a22ef7b1ef51d0d9c6ea53a90ba52d9cd54c1791
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-05 10:37:33 +0700

    force use https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewPulse', function (User $user) {
             return $user->hasRole('super_admin'); 
         });
+
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
