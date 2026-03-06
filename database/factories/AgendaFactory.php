fd92bf1730f585f270a1cbd195de89477439f21d
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-07 03:53:34 +0700

    add link_grup faker

--- database/factories/AgendaFactory.php
+++ database/factories/AgendaFactory.php
@@ -25,7 +25,7 @@ class AgendaFactory extends Factory
             'location' => $this->faker->randomElement(['Online', 'Offline - Jember']),
             'is_free' => true,
             'quota' => $this->faker->numberBetween(50, 200),
-            'link' => $this->faker->url(),
+            'link_grup' => $this->faker->url(),
         ];
     }
 }
