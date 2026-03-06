595c4e9508c76ca53ca33ae5e1127aadcf129674
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-06 07:07:43 +0700

    create model agenda

---
+++ app/Models/Agenda.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agenda extends Model
+{
+    /** @use HasFactory<\Database\Factories\AgendaFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'is_free' => 'boolean',
+    ];
+}
