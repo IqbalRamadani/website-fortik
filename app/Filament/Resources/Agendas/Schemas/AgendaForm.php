092b805a5d45c5b569b75acdb800f73f1948a20c
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-06 07:09:19 +0700

    create resource agenda

---
+++ app/Filament/Resources/Agendas/Schemas/AgendaForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Agendas\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\Toggle;
+
+class AgendaForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                TextInput::make('level')
+                    ->required()
+                    ->default('Nasional'),
+                FileUpload::make('banner_image')
+                    ->image()
+                    ->disk('public')
+                    ->directory('agenda-banners'),
+                DatePicker::make('start_date')
+                    ->required(),
+                DatePicker::make('end_date')
+                    ->required(),
+                TextInput::make('location')
+                    ->required(),
+                Toggle::make('is_free')
+                    ->default(true),
+                TextInput::make('price')
+                    ->numeric(),
+                TextInput::make('quota')
+                    ->numeric()
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Agendas/Tables/AgendasTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Agendas\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AgendasTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('banner_image'),
+                TextColumn::make('title')->searchable(),
+                TextColumn::make('start_date')->date(),
+                TextColumn::make('quota'),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
