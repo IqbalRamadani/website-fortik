09ab291f30739637b041d9d6848c0c94d85dc6a3
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-06 07:08:23 +0700

    create migration agenda

---
+++ database/migrations/2026_03_06_050954_create_agendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('level')->default('Nasional'); // Nasional, Internasional, dll
+            $table->string('banner_image')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('location')->default('Online');
+            $table->boolean('is_free')->default(true);
+            $table->decimal('price', 10, 2)->nullable();
+            $table->integer('quota')->default(100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendas');
+    }
+};
