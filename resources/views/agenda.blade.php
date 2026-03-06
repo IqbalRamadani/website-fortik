6ec11e62dfd7e0f755c3b9166e669016a2a6c1b1
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-06 15:10:09 +0700

    add agenda link

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
-    <div class="w-full h-screen flex flex-col justify-center items-center bg-linear-to-r from-lkoamaru to-koamaru lg:flex-row">
+    <livewire:agenda></livewire:agenda>
+    {{-- <div class="w-full h-screen flex flex-col justify-center items-center bg-linear-to-r from-lkoamaru to-koamaru lg:flex-row">
         <h1 class="font-bold text-2xl md:text-3xl p-8 text-center text-gray-200">Halaman sedang dalam proses pengembangan</h1>
         <img src="{{ asset('images/fortik-bot-1.webp') }}" alt="bot fortik" class="size-20 md:size-24">
-    </div>
+    </div> --}}
 </x-layout>
