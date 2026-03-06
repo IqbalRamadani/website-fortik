a0ade43c5b218e2691479c7885ef8aa66fdf91de
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-06 14:44:39 +0700

    add link faker

--- database/factories/AgendaFactory.php
+++ database/factories/AgendaFactory.php
@@ -25,6 +25,7 @@ class AgendaFactory extends Factory
             'location' => $this->faker->randomElement(['Online', 'Offline - Jember']),
             'is_free' => true,
             'quota' => $this->faker->numberBetween(50, 200),
+            'link' => $this->faker->url(),
         ];
     }
 }
