ee31f65e00d1fff47ba10c29c621fc56e9cc0072
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-09 12:20:52 +0700

    remove mt-4 from navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -29,7 +29,7 @@
             class="w-full md:!block md:w-auto md:!h-auto" 
             x-cloak>
             
-            <ul class="flex flex-col font-semibold text-sm lg:text-base p-0 mt-4 bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-4 lg:mt-0 md:border-0">
+            <ul class="flex flex-col font-semibold text-sm lg:text-base p-0 bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:border-0">
                 <li>
                     <x-nav-link href="/" :active="request()->is('/')">BERANDA</x-nav-link>
                 </li>
