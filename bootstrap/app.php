8307664ebfd3b9ddc1099d05db3b5dbe9a6aa226
IqbalRamadani <hakimramadani24@gmail.com>
2026-03-16 07:35:13 +0700

    edit trustproxies to all

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $proxies = env('TRUSTED_PROXIES');
-
-        if ($proxies) {
-            $middleware->trustProxies(at: explode(',', $proxies));
-        }
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
